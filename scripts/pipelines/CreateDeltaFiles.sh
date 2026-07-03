47d9ac357b933dbd1aec08a321bc74bcaee8ebc0
Sris <srii.seelam@merkle.com>
2026-07-04 00:04:12 +1200

    sfdx projects

--- scripts/pipelines/CreateDeltaFiles.sh
+++ scripts/pipelines/CreateDeltaFiles.sh
@@ -42,6 +42,8 @@ echo "" # insert new line
 IGNORE_FILE="./ignorefolders"
 # Write auth file safely
 printf '%s' "$IGNORE_PATHS" > "$IGNORE_FILE"
+echo "For Deployment - Contents of Ignorefile"
+cat $IGNORE_FILE
 sf sgd source delta -f $FROM_TAG -t $TO_TAG -o changedSources --generate-delta -i $IGNORE_FILE -a $API_VERSION
 echo "" # insert new line
 echo "For Deployment - Contents of changedSources/package/package.xml with ignore file:"

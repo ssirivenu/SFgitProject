07fc93f5df5265f27d02c2b60d449418404230dd
Srii Seelam <srii.seelam@merkle.com>
2026-07-15 06:18:43 +1200

    hj

--- scripts/pipelines/CreateDeltaFiles.sh
+++ scripts/pipelines/CreateDeltaFiles.sh
@@ -8,10 +8,8 @@
 mkdir changedSources
 # Get Delta using the Sf git delta plugin 
 echo "" # insert new line
-echo "FROM_TAG=$FROM_TAG"
-echo "TO_TAG=$TO_TAG"
 #echo "FROM_TAG: $FROM_TAG" # if commented, HEAD~1 is used as the from commit
-sf sgd source delta -f $FROM_TAG -t $TO_TAG -o "changedSources" -i .forceignore -a $API_VERSION --generate-delta
+sf sgd source delta -f $FROM_TAG -t $TO_TAG -o changedSources --generate-delta -i .forceignore -a $API_VERSION
 #sf sgd source delta -f HEAD~1 -t HEAD -o "changedSources" -i .forceignore -a $API_VERSION
 echo "" # insert new line
 echo "For Deployment - Contents of changedSources/package/package.xml:"
@@ -20,45 +18,3 @@ echo "" # insert new line
 echo "Destructive Changes in changedSources/destructiveChanges/destructiveChanges.xml:"
 cat changedSources/destructiveChanges/destructiveChanges.xml
 echo "" # insert new line
-#!/bin/bash     
-#Srii Seelam 2024-JUN-07- Initial Version
-# Deploy or Validate incremental changes to the org
-# Script Arguments:
-# $1 = if null, do actual deployment to Org, else do a validation only
-
-
-IGNORE_FILE="./ignorefolders"
-# Write auth file safely
-echo "$IGNORE_PATHS"
-printf '%s' "$IGNORE_PATHS" > "$IGNORE_FILE"
-echo "For Deployment - Contents of Ignorefile"
-cat "$IGNORE_FILE"
-sf sgd source delta -f $FROM_TAG -t $TO_TAG -o changedSources --generate-delta -i $IGNORE_FILE -a $API_VERSION
-echo "" # insert new line
-echo "For Deployment - Contents of changedSources/package/package.xml with ignore file:"
-cat changedSources/package/package.xml
-
-set -x
-
-SOURCE_ARGS=()
-while IFS= read -r path; do
-    path="${path//$'\r'/}"
-    [ -n "$path" ] && SOURCE_ARGS+=(-s "$path")
-done <<< "$IGNORE_PATHS"
-
-echo "Generated source arguments:"
-printf '%q ' "${SOURCE_ARGS[@]}"
-echo
-
-sf sgd source delta \
-    -f "$FROM_TAG" \
-    -t "$TO_TAG" \
-    -o changedSources \
-    "${SOURCE_ARGS[@]}" \
-    -a "$API_VERSION"
-
-set +x
-
-echo
-echo "For Deployment - Contents of changedSources/package/package.xml:"
-cat changedSources/package/package.xml

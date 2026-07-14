270f0f90e3bef93dbc0069580d861b3cebbeb66a
Srii Seelam <srii.seelam@merkle.com>
2026-07-15 07:48:34 +1200

    ds

--- scripts/pipelines/AuthorizeOrg.sh
+++ scripts/pipelines/AuthorizeOrg.sh
@@ -37,5 +37,5 @@ openssl enc -aes-256-cbc --md sha1 -nosalt -base64 -d -in $ENCRYPTED_FILE  -out 
 set -x 
 # 7. Authorize Salesforce org via JWT flow (Execution is inherently safe without 'set -x')
 echo "🚀 Initializing JWT OAuth flow with Salesforce..."
-sf org login jwt -o $USER_NAME  -f $SERVER_KEY -i "3MVG9JgJamLaOK52YAPdElwPr.x52bQjcB9h_wJFbU6svmTnVSXyYlAgpHWG17xudTms0oON6ga1mLGYcvKM5" -r https://test.salesforce.com -a $SANDBOX_NAME
+sf org login jwt -o $USER_NAME  -f $SERVER_KEY -i $CONSUMER_KEY -r $INSTANCE_URL -a $SANDBOX_NAME
 set +x
